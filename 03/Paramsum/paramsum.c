c98b80af26d2ae4aad154891e0134ed3dee98f30
Alexan ANDERSON <alanders@c3r6s1.wethinkcode.co.za>
2019-09-03 09:12:00 +0200

    Paramsum + Trace + Subject

---
+++ 03/Paramsum/paramsum.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alanders <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/03 09:06:01 by alanders          #+#    #+#             */
+/*   Updated: 2019/09/03 09:08:42 by alanders         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putposnbr(int n)
+{
+	if (n > 9)
+		ft_putposnbr(n / 10);
+	ft_putchar(n % 10 + '0');
+}
+
+int	main(int argc, char **argv)
+{
+	argv = 0;
+	ft_putposnbr(argc - 1);
+	write(1, "\n", 1);
+	return (0);
+}
